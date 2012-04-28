fbf619db9deb30ac46c9ffa72ebc7646f3292c20
Suneth <suneth@mint>
2012-04-29 00:13:14 +0100

    update

--- configure/init.sh
+++ configure/init.sh
@@ -9,7 +9,7 @@ fi
 cache=https://raw.github.com/rvaronos/code-gathering/master/configure/
 conf=/root/.gconf/apps/gnome-terminal/
 search=Profile
-max=0
+max=-1
 # Install configurations for the given username
 user=$1
 
@@ -19,6 +19,14 @@ return | awk -v a="$1" -v b="$2" 'BEGIN{print index(a,b)}'
 substr(){
 return | awk -v s="$1" -v a="$2" 'BEGIN{print substr(s,a)}'
 }
+substrl(){
+return | awk -v s="$1" -v a="$2" -v b="$3" 'BEGIN{print substr(s,a,b)}'
+}
+replace(){
+return | awk -v  -v regex="$2" replacement="$3" target="$3" 'BEGIN{print sub(regex,replacement,target)}'
+}
+
+
 
 cd $conf
 mkdir -p profiles
@@ -57,20 +65,30 @@ wget -N ${cache}global.xml
 mv global.xml %gconf.xml
 fi
 
-line="\n<li type=\"string\">\n<stringvalue>Profile${index}</stringvalue>\n</li>"
-lineNum=$(grep -n '<entry name="profile_list"' %gconf.xml | awk -F: '{print $1}')
- 
-sed "$lineNum a\
-> ${line}" %gconf.xml > newfile
-mv newfile %gconf.xml
+gconftool-2 --shutdown
 
+search=Profile${index}
+list=$(gconftool-2 --get "/apps/gnome-terminal/global/profile_list")
+i=$(indexOf $list $search)
+
+if [ $i = 0 ]
+then
+	list=[${search},$( substr $list 2 ) 
+fi
+
+gconftool-2 -s /apps/gnome-terminal/global/profile_list -t list --list-type string $list
+
+gconftool-2 --spawn
 
 cd ~/.cache
 mkdir -p code-gathering
 cd code-gathering
 wget -N ${cache}packages ${cache}script.sh ${cache}profile.xml
 
-sed -i 's/\${ID}/${index}/g' profile.xml
-#gconftool-2 --load profile.xml
-#gnome-terminal --window-with-id=${index} -e "sh script.sh $USER"
+sed -i "s/\${ID}/${index}/g" profile.xml
+gconftool-2 --load profile.xml
+
+gconftool-2 --spawn
+
+#gnome-terminal --window-with-profile=CodeGathering -e "sh script.sh $USER"
 
